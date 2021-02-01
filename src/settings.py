d1fb6bf7665b6a91b9638a92ea25f624a5dd9200
Jayson Gonzaga <Jayson Gonzaga@DESKTOP-L6O1VH5>
2021-02-01 19:45:28 +0800

    fix js

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import mimetypes
 
 mimetypes.add_type("text/css", ".css", True)
-mimetypes.add_type("text/js", ".js", True)
+mimetypes.add_type("text/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

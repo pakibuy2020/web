d31457ca313a91ca73bcd09064fc1491f887d52b
Jayson Gonzaga <Jayson Gonzaga@DESKTOP-L6O1VH5>
2021-02-01 17:07:05 +0800

    fix static

--- src/settings.py
+++ src/settings.py
@@ -185,9 +185,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # GRAPH_MODELS = {
 #     'all_applications': True,
